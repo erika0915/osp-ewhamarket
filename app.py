740c6ad99bf164997410d276b3f5263e7531f955
smileblu <dlalth5610@gmail.com>
2024-11-13 13:52:15 +0900

    수정

--- app.py
+++ app.py
@@ -1,38 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect, url_for, session
+from database import DBhandler
+import hashlib
+import sys
 
 application  = Flask(__name__)
+application.config["SECRET_KEY"] = "helloosp"
 
-@application.route('/page1')
-def page1():
-    return render_template('page1.html')
+DB = DBhandler()
 
-@application.route('/page2')
-def page2():
-    return render_template('page2.html')
+@application.route("/")
+def hello():
+    return render_template("index.html")
 
-@application.route('/page3')
-def page3():
-    return render_template('page3.html')
+@application.route("/login")
+def login():
+    return render_template("login.html")
 
-@application.route('/page4')
-def page4():
-    return render_template('page4.html')
+@application.route("/signup")
+def signup():
+    return render_template("signup.html")
 
-@application.route('/page5')
-def page5():
-    return render_template('page5.html')
-
-@application.route('/page6')
-def page6():
-    return render_template('page6.html')
-
-@application.route('/page7')
-def page7():
-    return render_template('page7.html')
-
-@application.route('/page8')
-def page8():
-    return render_template('page8.html')
+@application.route("/signup_post", methods=['POST'])
+def register_user():
+    data=request.form
+    pw=request.form['pw']
+    pw_hash = hashlib.sha256(pw.encode('utf-8')).hexdigest()
+    if DB.insert_user(data,pw_hash):
+        return render_template("login.html")
+    else:
+        flash("user id already exist!")
+        return render_template("signup.html")
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0', debug=True)
