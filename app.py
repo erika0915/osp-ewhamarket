95b534bb1a39e403717476ed6e49bc49ae7b6264
youn1205 <lovegrace1205@gmail.com>
2024-11-20 14:19:56 +0900

    그룹과제1과 합침

--- app.py
+++ app.py
@@ -82,6 +82,24 @@ def view_item_detail(name):
 def login():
     return render_template("login.html")
 
+@application.route("/login_confirm", methods=['POST'])
+def login_user():
+    id_=request.form['id']
+    pw=request.form['pw']
+    pw_hash = hashlib.sha256(pw.encode('utf-8')).hexdigest()
+    if DB.find_user(id_,pw_hash):
+        session['id']=id_
+        return redirect(url_for('view_list'))
+    else:
+        flash("Wrong ID or PW!")
+        return render_template("login.html")
+
+@application.route("/logout")
+def logout_user():
+    session.clear()
+    return redirect(url_for('view_list'))
+
+
 @application.route("/signup")
 def signup():
     return render_template("signup.html")
