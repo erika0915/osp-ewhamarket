8c243a0f6a8287932953d375c3cefb14abb7eec1
erika0915 <soohee2001@gmail.com>
2024-11-21 15:35:37 +0900

    Your updated commit message

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ import json
 
 class DBhandler:
     def __init__(self):
-        with open('.github/authentication/firebase_auth.json') as f:
+        with open('authentication/firebase_auth.json') as f:
             config = json.load(f)
 
         firebase = pyrebase.initialize_app(config)
