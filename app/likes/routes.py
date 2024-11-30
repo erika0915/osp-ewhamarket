0a0f63e6f97257343653d2931caf82c5bc5b0efc
erika0915 <soohee2001@gmail.com>
2024-11-30 15:42:06 +0900

    chore : likes에서도 userId 로 변경

--- app/likes/routes.py
+++ app/likes/routes.py
@@ -4,29 +4,29 @@ from . import likes_bp
 # 좋아요 상태 조회
 @likes_bp.route('/show_heart/<productName>/', methods=['GET'])
 def show_heart(productName):
-    user_id = session.get('id')
-    if not user_id:
+    userId = session.get('userId')
+    if not userId:
         return jsonify({'error': '로그인이 필요합니다!'}), 401
 
-    my_heart = likes_bp.db.get_heart_byname(user_id, productName)
+    my_heart = likes_bp.db.get_heart_byname(userId, productName)
     return jsonify({'my_heart': my_heart})
 
 # 좋아요 등록
 @likes_bp.route('/like/<productName>/', methods=['POST'])
 def like(productName):
-    user_id = session.get('id')
-    if not user_id:
+    userId = session.get('userId')
+    if not userId:
         return jsonify({'error': '로그인이 필요합니다!'}), 401
 
-    likes_bp.db.update_heart(user_id, 'Y', productName)
+    likes_bp.db.update_heart(userId, 'Y', productName)
     return jsonify({'msg': '좋아요 완료!'})
 
 # 좋아요 취소
 @likes_bp.route('/unlike/<productName>/', methods=['POST'])
 def unlike(productName):
-    user_id = session.get('id')
-    if not user_id:
+    userId = session.get('userId')
+    if not userId:
         return jsonify({'error': '로그인이 필요합니다!'}), 401
 
-    likes_bp.db.update_heart(user_id, 'N', productName)
+    likes_bp.db.update_heart(userId, 'N', productName)
     return jsonify({'msg': '좋아요 취소 완료!'})
